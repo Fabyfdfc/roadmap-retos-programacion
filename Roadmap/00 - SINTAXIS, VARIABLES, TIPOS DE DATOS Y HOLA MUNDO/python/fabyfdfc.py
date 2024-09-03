7163f618cf21edf57aecdbd3a5003094878f0cf1
Fabyfdfc <fabianandradesandoval@gmail.com>
2024-09-03 13:21:16 +0200

    fabyfdfc.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/fabyfdfc.py
@@ -0,0 +1,17 @@
+"Hola A todos"
+
+"Adios"
+
+my_variable = "Mi variable"
+
+MY_CONSTANT = "Mi constante"
+
+my_int = 1
+my_float = 1.2
+my_bool = True
+my_bool = False
+my_string = "cadena"
+my_other_string = "otra cadena"
+
+print ("Hola, Phyton!")
+
